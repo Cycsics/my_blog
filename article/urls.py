afe85d86f9f732f300f3b4d7ef05b259cc3c954f
Cycsics <sz597002139@outlook.com>
2020-10-28 21:51:34 +0800

    first commit

---
+++ article/urls.py
@@ -0,0 +1,22 @@
+# 引入path
+from django.urls import path
+from . import views
+
+# 正在部署的应用的名称
+app_name = 'article'
+
+urlpatterns = [
+    # path函数将url映射到视图
+    path('article-list/', views.article_list, name='article_list'),
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+    # 写文章
+    path('article-create/', views.article_create, name='article_create'),
+    # 安全删除文章
+    path(
+        'article-safe-delete/<int:id>/',
+        views.article_safe_delete,
+        name='article_safe_delete'
+    ),
+    # 更新文章
+    path('article-update/<int:id>/', views.article_update, name='article_update'),
+]
