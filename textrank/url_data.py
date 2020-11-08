bddae0edd3fd8cbe387be005f343edf24c5fc6de
Cycsics <sz597002139@outlook.com>
2020-11-08 22:40:32 +0800

    commit url_data modle

--- textrank/url_data.py
+++ textrank/url_data.py
@@ -2,25 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 import time
-import tkinter
-import os
-from tkinter import *
-from tkinter.filedialog import askdirectory
-from tkinter import messagebox
-from tkinter import ttk
-from tkinter import scrolledtext
-from tkinter import END
 from bs4 import BeautifulSoup
-import datetime
-import threading
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.common.action_chains import ActionChains
-from webdriver_manager.chrome import ChromeDriverManager
 from summa.summarizer import summarize
 from summa.keywords import keywords
 import requests
